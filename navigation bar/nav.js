c924393cfcf64212a51083dfbcff1e16f47e73af
Hassan Kheireddin <hassankheireddin0@gmail.com>
2025-01-04 03:48:39 +0200

    update home page

--- navigation bar/nav.js
+++ navigation bar/nav.js
@@ -11,8 +11,8 @@ function insertNavbar() {
             <input type="text" placeholder="Search Everything..." class="search-input">
         </div>
         <ul class="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Courses</a></li>
+            <li><a href="../home/home.html">Home</a></li>
+            <li><a href="#">Learn</a></li>
             <li><a href="#">About Us</a></li>
             <li><a href="../contact us/contact-us.html">Contact Us</a></li>
             <li><a href="../signup/signup.html">Sign Up</a></li>
