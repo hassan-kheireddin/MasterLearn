37d4a84168ac8823be109e64ef6569d8d138ac17
Hassan Kheireddin <hassankheireddin0@gmail.com>
2025-01-06 19:54:11 +0200

    finished

--- navigation bar/nav.js
+++ navigation bar/nav.js
@@ -6,17 +6,20 @@ function insertNavbar() {
                 <img src="../images/logo.png" alt="logo" class="logo-img">
             </a>
         </div>
-        <div class="search-bar">
-            <span class="search-btn"></span>
-            <input type="search" placeholder="Search Everything..." class="search-input">
-        </div>
+
         <ul class="nav-links">
             <li><a href="../home/home.html">Home</a></li>
             <li><a href="../courses/courses.html">Learn</a></li>
             <li><a href="../about/about.html">About</a></li>
             <li><a href="../contact us/contact-us.html">Contact</a></li>
+            </ul>
+        <div class="search-bar">
+            <span class="search-btn"></span>
+            <input type="search" placeholder="Search Everything..." class="search-input">
+        </div>
+        <ul class="nav-links">
             <li><a href="../signup/signup.html">Sign Up</a></li>
-            <li><a href="../login/login.html">Log In</a></li>
+            <li><a href="../login/login.html" >Log In</a></li>
         </ul>
     </nav>
     `;
