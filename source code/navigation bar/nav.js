aa597e32e6286bab251d7a9b57be0dc492857d2c
Hassan Kheireddin <hassankheireddin0@gmail.com>
2025-02-06 13:11:50 +0200

    finish

--- source code/navigation bar/nav.js
+++ source code/navigation bar/nav.js
@@ -12,10 +12,10 @@ function insertNavbar() {
             <ul class="nav-links">
                 <li><a href="../courses/courses.html" style="margin-left: 20px;">Courses</a></li>
                 <li><a href="../documents/document.html">Documents</a></li>
-                <li><a href="#">Road Map</a><li>
+                <li><a href="../roadmap/roadmap.html">Road Map</a><li>
                 <li><a href="../community/community.html" style="margin-left: 55px;">Community</a></li>
-                <li><a href="../about/about.html">About</a></li>
                 <li><a href="../contact us/contact-us.html">Contact</a></li>
+                <li><a href="../about/about.html">About</a></li>
             </ul>
         </div>
         <div class="nav-right">
